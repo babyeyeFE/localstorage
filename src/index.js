5bafc912a58e2612327deba72d438842bdd9d3da
melodyVoid <botaoxyz@gmail.com>
2018-11-11 11:57:25 +0800

    feat(src): 修改 JSON.parse 解析报错的 bug

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const stub = require('./stub')
+const ls = 'localStorage' in global && global.localStorage ? global.localStorage : stub
+
+function accessor (key, value) {
+  if (arguments.length === 1) {
+    return get(key)
+  }
+  return set(key, value)
+}
+
+function get (key) {
+  const value = ls.getItem(key)
+  let parseValue
+  try {
+    parseValue = JSON.parse(value)
+  } catch (error) {
+    parseValue = value
+  }
+  return parseValue
+}
+
+function set (key, value) {
+  try {
+    ls.setItem(key, JSON.stringify(value))
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+function remove (key) {
+  return ls.removeItem(key)
+}
+
+function clear () {
+  return ls.clear()
+}
+
+accessor.set = set
+accessor.get = get
+accessor.remove = remove
+accessor.clear = clear
+
+module.exports = accessor
